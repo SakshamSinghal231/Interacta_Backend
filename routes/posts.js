eda9a3ebd66b83c8121567d98df32d7a19c0d08d
SakshamSinghal231 <sakshamsinghal231@gmail.com>
2023-08-11 01:32:09 +0530

    first commit

---
+++ routes/posts.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { commentPost, getFeedPosts, getUserPosts, likePost } from "../controllers/posts.js";
+import { verifyToken } from "../middleware/auth.js";
+
+const router = express.Router();
+
+/* READ */
+router.get("/", verifyToken, getFeedPosts);
+router.get("/:userId/posts", verifyToken, getUserPosts);
+router.get
+
+/* UPDATE */
+router.patch("/:id/like", verifyToken, likePost);
+router.post("/:id/comment", verifyToken, commentPost)
+export default router;
